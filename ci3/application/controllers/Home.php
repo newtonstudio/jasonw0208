71587d3b13bed627a762af7b360b22c05a078613
Jason Tian <jason.tian@i-tea.com.tw>
2020-08-04 10:45:39 +0800

    Update Album Model

--- ci3/application/controllers/Home.php
+++ ci3/application/controllers/Home.php
@@ -21,5 +21,47 @@ class Home extends CI_Controller {
         $this->load->view("footer");
 
     }
+
+    public function test(){
+
+        $this->load->model("Album_model");
+
+        //test insert
+        // $this->Album_model->insert([
+        //     'title' => "Album A",
+        //     'image_url' => NULL,
+        //     'description' => "This is album A",
+        //     'minutes' => "10 min",
+        //     'created_date' => date("Y-m-d H:i:s"),
+        // ]);
+        
+        //test update
+        // $this->Album_model->update([
+        //     'id' => 2
+        // ], [
+        //     'title' => "Album B",
+        //     'modified_date' => date("Y-m-d H:i:s"),
+        // ]);
+
+        //test delete
+        // $this->Album_model->delete([
+        //     'id' => 3
+        // ]);
+
+        //test get_where
+        // $datalist = $this->Album_model->get_where([
+        //     'is_deleted' => 0,
+        // ]);
+        // print_r($datalist);
+
+        //test getOne
+        // $data = $this->Album_model->getOne([
+        //      'is_deleted' => 0,
+        // ]);
+        // print_r($data);
+    
+
+
+    }
 }
 ?>
