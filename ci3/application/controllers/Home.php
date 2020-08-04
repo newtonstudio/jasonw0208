0ae124e28d8afdf3f2062945ae33d5fc3497bc59
Jason Tian <jason.tian@i-tea.com.tw>
2020-08-04 12:33:59 +0800

    Pagination library

--- ci3/application/controllers/Home.php
+++ ci3/application/controllers/Home.php
@@ -37,42 +37,8 @@ class Home extends CI_Controller {
             'is_deleted' => 0,
         ]);
 
-        $this->load->library("pagination");
-        $config['base_url'] = base_url('album/'.$id.'/'.$detail['title']);
-        $config['total_rows'] = $total_rows;
-        $config['per_page'] = $item_per_page;
-        $config['use_page_numbers'] = true;
-        $config['attributes'] = array('class' => 'page-link');
-
-        $config['full_tag_open'] = '<nav aria-label="Page navigation example"><ul class="pagination">';
-        $config['full_tag_close'] = '</ul></nav>';
-
-        $config['first_tag_open'] = '<li class="page-item">';
-        $config['first_tag_close'] = '</li>';
-        $config['first_link'] = 'First';
-
-        $config['last_tag_open'] = '<li class="page-item">';
-        $config['last_tag_close'] = '</li>';
-        $config['last_link'] = 'Last';
-
-        $config['prev_link'] = '&lt;';
-        $config['prev_tag_open'] = '<li class="page-item">';
-        $config['prev_tag_close'] = '</li>';
-
-        $config['next_link'] = '&gt;';
-        $config['next_tag_open'] = '<li class="page-item">';
-        $config['next_tag_close'] = '</li>';
-
-        $config['cur_tag_open'] = '<li class="page-item active"><a href="#" class="page-link">';
-        $config['cur_tag_close'] = '</a></li>';
-
-        $config['num_tag_open'] = '<li class="page-item">';
-        $config['num_tag_close'] = '</li>';
-        
-
-
-        $this->pagination->initialize($config);
-        $this->data['pagination'] = $this->pagination->create_links();
+        $this->load->library("mypagination");        
+        $this->data['pagination'] = $this->mypagination->getPagination(base_url('album/'.$id.'/'.$detail['title']), $total_rows, $item_per_page);
         
 
         $this->load->view("header");
