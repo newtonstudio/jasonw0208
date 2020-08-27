7ca3a90dbc696943e416dca3b2aaee8ded4aab27
Jason Tian <jason.tian@i-tea.com.tw>
2020-08-27 12:07:51 +0800

    Update Api_event_manage.php

--- ci3/application/controllers/Api_event_manage.php
+++ ci3/application/controllers/Api_event_manage.php
@@ -3,6 +3,10 @@ class Api_event_manage extends CI_Controller {
 
     public function backup(){
 
+        header('Content-Type: application/json; charset=utf-8');
+        header("Access-Control-Allow-Origin: *");
+        header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+
         echo json_encode([
             'status' => "OK",
             'result' => "1",
