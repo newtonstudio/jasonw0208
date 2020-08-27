ffd4bb8a48e85f856ec67f158d25a389bf0ce72e
Jason Tian <jason.tian@i-tea.com.tw>
2020-08-27 12:30:43 +0800

    Update Api_event_manage.php

--- ci3/application/controllers/Api_event_manage.php
+++ ci3/application/controllers/Api_event_manage.php
@@ -1,6 +1,50 @@
 <?php
 class Api_event_manage extends CI_Controller {
 
+    public function restore(){
+
+        header('Content-Type: application/json; charset=utf-8');
+        header("Access-Control-Allow-Origin: *");
+        header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+
+
+	    if(isset($_SERVER["CONTENT_TYPE"]) && strpos($_SERVER["CONTENT_TYPE"], "application/json") !== false) {
+            $_POST = array_merge($_POST, (array) json_decode(trim(file_get_contents('php://input')), true));
+
+            $name = $this->input->post("name", true);
+            $password = $this->input->post("password", true);
+
+            $this->load->model("Eventbackup_model");
+
+            $eventData = $this->Eventbackup_model->getOne([
+                'name' => $name,
+                'password' => $password,
+                'is_deleted' => 0,
+            ]);
+
+            if(!empty($eventData)) {
+
+                echo json_encode([
+                    'status' => "OK",
+                    'result' => [
+                        'eventList' => $eventData['eventList'],
+                        'profile'   => $eventData['profile'],
+                    ],
+                ]);
+
+            } else {
+
+                echo json_encode([
+                    'status' => "ERROR",
+                    'result' => "Invalid Name or password",
+                ]);
+
+            }
+                
+        }
+
+    }
+
     public function backup(){
 
         header('Content-Type: application/json; charset=utf-8');
